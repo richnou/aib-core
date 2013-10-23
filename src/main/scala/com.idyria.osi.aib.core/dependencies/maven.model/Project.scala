cf1b4f3d7c8ae850516099f6de28bd2773886bed
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-10-23 13:48:31 +0200

    Improved maven model Project definition class to be able to easily find back the actual artifact pom.xml (from classloader packaged jar or developement folder), parse it and get the project identifying strings (like version)

--- src/main/scala/com.idyria.osi.aib.core/dependencies/maven.model/Project.scala
+++ src/main/scala/com.idyria.osi.aib.core/dependencies/maven.model/Project.scala
@@ -6,42 +6,94 @@ package com.idyria.osi.aib.core.dependencies.maven.model
 import com.idyria.osi.ooxoo.core.buffers.structural._
 import com.idyria.osi.ooxoo.core.buffers.datatypes.XSDStringBuffer
 import com.idyria.osi.aib.core.dependencies.ArtifactTrait
+import java.io.File
+import com.idyria.osi.ooxoo.core.buffers.structural.io.sax.StAXIOBuffer
+import java.io.FileInputStream
 
-
- 
 /**
  * @author rleys
  *
  */
-@xelement(name="project")
-class Project extends ElementBuffer  {
+@xelement(name = "project",ns="http://maven.apache.org/POM/4.0.0")
+class Project extends ElementBuffer {
 
-  /*var artifactId: XSDStringBuffer
-  var groupId: XSDStringBuffer
-  var version: XSDStringBuffer
-  var packaging : XSDStringBuffer*/
+  @xelement(ns="http://maven.apache.org/POM/4.0.0")
+  var artifactId: XSDStringBuffer = null
+  
+  @xelement(ns="http://maven.apache.org/POM/4.0.0")
+  var groupId: XSDStringBuffer = null
+  
+  @xelement(ns="http://maven.apache.org/POM/4.0.0")
+  var version: XSDStringBuffer = null
+  
+  @xelement(ns="http://maven.apache.org/POM/4.0.0")
+  var packaging : XSDStringBuffer = null
+  
 
   /**
    * <Dependencies>
    */
-  @xelement(name="dependencies")
-  var dependencies: XList[Dependencies] =  XList[Dependencies] { new Dependencies }
+  @xelement(name = "dependencies")
+  var dependencies: XList[Dependencies] = XList[Dependencies] { new Dependencies }
+
+}
+object Project {
+
+  /**
+   * Try to find a project definition from:
+   *
+   * Classloader META-INF/maven/groupId/artifactId/pom.xml
+   * Local file start: pom.xml
+   *
+   */
+  def apply(groupId: String, artifactId: String): Option[Project] = {
+
+    // Look for possibilities
+    var url = Thread.currentThread().getContextClassLoader().getResource(s"META-INF/maven/$groupId/$artifactId/pom.xml")
+    var file = new File("pom.xml")
+
+    // Resolve
+    (url, file) match {
+      case (null, null) => None
+
+      // Use URL
+      case (url, null)  =>
+      	
+        var project = new Project()
+        project.appendBuffer(new StAXIOBuffer(url.openStream()))
+        project.lastBuffer.streamIn
+        
+        Some(project)
+        
+      // Use File
+      case (url, file)  =>
+        
+        var project = new Project()
+        project.appendBuffer(new StAXIOBuffer(new FileInputStream(file)))
+        project.lastBuffer.streamIn
+        
+        Some(project)
+        
+    }
+
+
+  }
 
 }
 
-@xelement(name="dependencies")
+@xelement(name = "dependencies")
 class Dependencies extends ElementBuffer {
 
   /**
    * <Dependency>
    */
-  @xelement(name="dependency")
+  @xelement(name = "dependency")
   var dependency = XList[Dependency] { new Dependency }
 
 }
 
-@xelement(name="dependency")
-class Dependency extends ElementBuffer  with ArtifactTrait[XSDStringBuffer] {
+@xelement(name = "dependency")
+class Dependency extends ElementBuffer with ArtifactTrait[XSDStringBuffer] {
 
   //
   var artifactId: XSDStringBuffer = null
