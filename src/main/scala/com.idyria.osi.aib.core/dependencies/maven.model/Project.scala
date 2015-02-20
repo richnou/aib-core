155cad0352c97eedbbaf472de13a86c13c40d7a8
richnou <richard.leys@kit.edu>
2015-02-20 18:19:05 +0100

    Bumped Scala to 2.11.5, improject Maven Model fast parser

--- src/main/scala/com.idyria.osi.aib.core/dependencies/maven.model/Project.scala
+++ src/main/scala/com.idyria.osi.aib.core/dependencies/maven.model/Project.scala
@@ -14,91 +14,110 @@ import java.io.FileInputStream
  * @author rleys
  *
  */
-@xelement(name = "project",ns="http://maven.apache.org/POM/4.0.0")
+@xelement(name = "project", ns = "http://maven.apache.org/POM/4.0.0")
 class Project extends ElementBuffer {
 
-  @xelement(ns="http://maven.apache.org/POM/4.0.0")
-  var artifactId: XSDStringBuffer = null
-  
-  @xelement(ns="http://maven.apache.org/POM/4.0.0")
-  var groupId: XSDStringBuffer = null
-  
-  @xelement(ns="http://maven.apache.org/POM/4.0.0")
-  var version: XSDStringBuffer = null
-  
-  @xelement(ns="http://maven.apache.org/POM/4.0.0")
-  var packaging : XSDStringBuffer = null
-  
+    @xelement(ns = "http://maven.apache.org/POM/4.0.0")
+    var artifactId: XSDStringBuffer = ""
 
-  /**
-   * <Dependencies>
-   */
-  @xelement(name = "dependencies")
-  var dependencies: XList[Dependencies] = XList[Dependencies] { new Dependencies }
+    @xelement(ns = "http://maven.apache.org/POM/4.0.0")
+    var groupId: XSDStringBuffer = ""
+
+    @xelement(ns = "http://maven.apache.org/POM/4.0.0")
+    var version: XSDStringBuffer = ""
+
+    @xelement(ns = "http://maven.apache.org/POM/4.0.0")
+    var packaging: XSDStringBuffer = null
+
+    /**
+     * <Dependencies>
+     */
+    @xelement(name = "dependencies")
+    var dependencies: Dependencies = null
 
 }
 object Project {
 
-  /**
-   * Try to find a project definition from:
-   *
-   * Classloader META-INF/maven/groupId/artifactId/pom.xml
-   * Local file start: pom.xml
-   *
-   */
-  def apply(groupId: String, artifactId: String): Option[Project] = {
+    def apply(url: java.net.URL) = {
 
-    // Look for possibilities
-    var url = Thread.currentThread().getContextClassLoader().getResource(s"META-INF/maven/$groupId/$artifactId/pom.xml")
-    var file = new File("pom.xml")
+        // Instanciate
+        var res = new Project
 
-    // Resolve
-    (url, file.exists()) match {
-      case (null, false) => None
+        // Set Stax Parser and streamIn
+        var io = com.idyria.osi.ooxoo.core.buffers.structural.io.sax.StAXIOBuffer(url)
+        res.appendBuffer(io)
+        io.streamIn
 
-      // Use URL
-      case (url, false)  =>
-      	
-        var project = new Project()
-        project.appendBuffer(new StAXIOBuffer(url.openStream()))
-        project.lastBuffer.streamIn
-        
-        Some(project)
-        
-      // Use File
-      case (_, true)  =>
-        
-        var project = new Project()
-        project.appendBuffer(new StAXIOBuffer(new FileInputStream(file)))
-        project.lastBuffer.streamIn
-        
-        Some(project)
-        
+        // Return
+        res
+
     }
 
+    /**
+     * Try to find a project definition from:
+     *
+     * Classloader META-INF/maven/groupId/artifactId/pom.xml
+     * Local file start: pom.xml
+     *
+     */
+    def apply(groupId: String, artifactId: String): Option[Project] = {
 
-  }
+        // Look for possibilities
+        var url = Thread.currentThread().getContextClassLoader().getResource(s"META-INF/maven/$groupId/$artifactId/pom.xml")
+        var file = new File("pom.xml")
+
+        // Resolve
+        (url, file.exists()) match {
+            case (null, false) => None
+
+            // Use URL
+            case (url, false) =>
+
+                var project = new Project()
+                project.appendBuffer(new StAXIOBuffer(url.openStream()))
+                project.lastBuffer.streamIn
+
+                Some(project)
+
+            // Use File
+            case (_, true) =>
+
+                var project = new Project()
+                project.appendBuffer(new StAXIOBuffer(new FileInputStream(file)))
+                project.lastBuffer.streamIn
+
+                Some(project)
+
+        }
+
+    }
 
 }
 
 @xelement(name = "dependencies")
 class Dependencies extends ElementBuffer {
 
-  /**
-   * <Dependency>
-   */
-  @xelement(name = "dependency")
-  var dependency = XList[Dependency] { new Dependency }
+    /**
+     * <Dependency>
+     */
+    @xelement(name = "dependency")
+    var dependency = XList[Dependency] { new Dependency }
 
 }
 
 @xelement(name = "dependency")
 class Dependency extends ElementBuffer with ArtifactTrait[XSDStringBuffer] {
 
-  //
-  var artifactId: XSDStringBuffer = null
+    @xelement(name = "artifactId")
+    var artifactId: XSDStringBuffer = ""
+    
+    @xelement(name = "groupId")
+    var groupId: XSDStringBuffer = ""
+    
+    @xelement(name = "version")
+    var version: XSDStringBuffer = ""
 
-  /*var groupId: XSDStringBuffer
+    /*var groupId: XSDStringBuffer
   var version: XSDStringBuffer
   var packaging : XSDStringBuffer*/
 
